198588c2d042297dc8f197ce90d7d7c02057fbeb
Fazli171 <matem3599798@gmail.com>
2026-03-12 22:20:43 +0500

    update

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = "8616718479:AAEwyY8YxWKdZbc73tp5HvOiOB7IRKu6nrg"
+
+YOUTUBE_NAME = "k views"
+
+ADMIN_ID = 5395280099
+
+# Kino saqlanadigan kanal
+CHANNEL_ID = -1003789928568
+
+# Youtube kanal
+YOUTUBE_CHANNEL = "https://www.youtube.com/@K_Rewiews"
